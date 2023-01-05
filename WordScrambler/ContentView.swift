f6b73fc62c7157ea45780844ec9f2c31494204a3
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2023-01-05 20:30:23 +0200

    Add files to project

---
+++ WordScrambler/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WordScrambler
+//
+//  Created by Danya Kukhar on 05.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by Danya Kukhar on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
